3b9dd444a42b690970ddaae101a9fa9edaebdfab
adyachenko <wolf.icefire@gmail.com>
2017-12-17 15:16:47 +0300

    Update urls.py

--- django_youtube/urls.py
+++ django_youtube/urls.py
@@ -1,28 +1,26 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('django_youtube.views',
+
+urlpatterns = (
     # list of the videos
-    url(r'^videos/?$', 'video_list', name="youtube_video_list"),
+    url(r'^videos/?$', 'django_youtube.views.video_list', name="youtube_video_list"),
 
     # video  display page, convenient to use in an iframe
-    url(r'^video/(?P<video_id>[\w.@+-]+)/$', 'video', name="youtube_video"),
+    url(r'^video/(?P<video_id>[\w.@+-]+)/$', 'django_youtube.views.video', name="youtube_video"),
 
     # upload page with a form
-    url(r'^upload/?$', 'upload', name="youtube_upload"),
+    url(r'^upload/?$', 'django_youtube.views.upload', name="youtube_upload"),
 
     # page that youtube redirects after upload
-    url(r'^upload/return/?$', 'upload_return', name="youtube_upload_return"),
+    url(r'^upload/return/?$', 'django_youtube.views.upload_return', name="youtube_upload_return"),
 
     # upload page with a form
-    url(r'^direct-upload/?$', 'direct_upload', name="youtube_direct_upload"),
+    url(r'^direct-upload/?$', 'django_youtube.views.direct_upload', name="youtube_direct_upload"),
 
     # remove video, redirects to upload page when it's done
-    url(r'^video/remove/(?P<video_id>[\w.@+-]+)/$', 'remove', name="youtube_video_remove"),
+    url(r'^video/remove/(?P<video_id>[\w.@+-]+)/$', 'django_youtube.views.remove', name="youtube_video_remove"),
 
     # check video availability, returns json response
-    url(r'^check-video-availability/(?P<video_id>[\w.@+-]+)$/?$', 'check_video_availability', name="youtube_check_video_availability"),
-    url(r'^video/(?P<video_id>[\w.@+-]+)/$', 'video', name="youtube_video"),
+    url(r'^check-video-availability/(?P<video_id>[\w.@+-]+)$/?$', 'django_youtube.views.check_video_availability', name="youtube_check_video_availability"),
+    url(r'^video/(?P<video_id>[\w.@+-]+)/$', 'django_youtube.views.video', name="youtube_video"),
 )
